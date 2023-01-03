1823b183328d23fe13295de8ebd3660b4338ca4a
favemma <ayoaborisade@yahoo.com>
2023-01-03 12:47:38 -0800

    Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 class LockedClass:
-
     """A locked class that only lets the user dynamically create the instance
-
         attribute 'first_name'"""
-
-	    __slots__ = ['first_name']
+    ___slots__ = ['first_name']
+    def __init__(self):
+        pass
